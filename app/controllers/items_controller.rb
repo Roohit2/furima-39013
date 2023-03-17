08e2ef6e28c422efdac34ee1646a5233d56a7b14
Roohit2 <v.p.lemonade@gmail.com>
2023-03-17 19:10:58 +0900

    リダイレクトのパスの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :update, :destroy]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -18,8 +17,10 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
+  
   def edit
+    @item = Item.find(params[:id])
+    contributor_confirmation
   end
 
   def show
@@ -29,8 +30,27 @@ class ItemsController < ApplicationController
   def destoroy
   end
 
+  def update
+
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render 'edit'
+    end
+  end
+
   private
+
   def item_params
     params.require(:item).permit(:image,:name,:description,:category_id,:status_id,:delivery_cost_id,:delivery_prefecture_id,:delivery_day_id,:price).merge(user_id: current_user.id)
   end
+
+  def contributor_confirmation
+    unless current_user.id == @item.user.id
+    redirect_to root_path
+    end  
+  end
+
 end
